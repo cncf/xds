01c74414265c350d2f91e2c3fc4af2873321a81b
Kunjan Patel <kunjanp@google.com>
2025-01-08 02:45:46 +0000

    Refactor generate_protobuf
    Signed-off-by: Kunjan Patel <kunjanp@google.com>

--- tools/generate_protobuf.py
+++ tools/generate_protobuf.py
@@ -13,22 +13,20 @@ def generate_lang_files_from_protos(language: str):
     output = os.path.join(workspace, language)
     bazel_bin = check_output(["bazel", "info", "bazel-bin"]).decode().strip()
 
-    protos = (
-        check_output(
+    protos = check_output(
             [
                 "bazel",
                 "query",
-                f'kind("{language}_proto_library", ...)',
+                {"go":'kind("go_proto_library", ...)',
+                 "python":'kind("py_proto_library", ...)'}[language]
             ]
-        )
-        .decode()
-        .split()
-    )
+        ).split()
     output_dir = f"github.com/cncf/xds/{language}"
     check_output(["bazel", "build", "-c", "fastbuild"] + protos)
-
+    print(protos)
     for rule in protos:
         rule_dir = rule.decode()[2:].rsplit(":")[0]
+        print("rule ", rule, rule_dir)
         input_dir = {
             "go": os.path.join(
                 bazel_bin, rule_dir, "pkg_go_proto_", "github.com/cncf/xds/go", rule_dir
@@ -39,15 +37,7 @@ def generate_lang_files_from_protos(language: str):
             "go": glob.glob(os.path.join(input_dir, "*.go")),
             "python": glob.glob(os.path.join(input_dir, "*_pb2.py")),
         }[language]
-        if language == "go":
-            input_dir = os.path.join(
-                bazel_bin, rule_dir, "pkg_go_proto_", "github.com/cncf/xds/go", rule_dir
-            )
-            input_files = glob.glob(os.path.join(input_dir, "*.go"))
-        elif language == "python":
-            input_dir = os.path.join(bazel_bin, rule_dir)
-            input_files = glob.glob(os.path.join(input_dir, "*_pb2.py"))
-
+        
         output_dir = os.path.join(output, rule_dir)
 
         # Ensure the output directory exists
