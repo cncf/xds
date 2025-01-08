e2c79e16e05396dbd3f2fff4249577afa578910a
Kunjan Patel <kunjanp@google.com>
2025-01-08 02:24:35 +0000

    Consolidate, setup.py with pyproject.toml and remove redundant license. Add disrtribution wheel
    Signed-off-by: Kunjan Patel <kunjanp@google.com>

--- tools/generate_protobuf.py
+++ tools/generate_protobuf.py
@@ -5,47 +5,58 @@ import glob
 import os
 import shutil
 
-def generate_go_protobufs(output,language):
-  bazel_bin = check_output(['bazel', 'info', 'bazel-bin']).decode().strip()
+workspace = check_output(["bazel", "info", "workspace"]).decode().strip()
+SUPPORTED_LANGUAGES = ["go", "python"]
 
-  if language == "go":
-    protos = check_output([
-        'bazel',
-        'query',
-        'kind("go_proto_library", ...)',
-    ]).split()
-    output_dir = 'github.com/cncf/xds/python'
-  if language == "python":
-    protos = check_output([
-        'bazel',
-        'query',
-        'kind("py_proto_library", ...)',
-    ]).split()
-    output_dir = 'github.com/cncf/xds/go'
-  check_output(['bazel', 'build', '-c', 'fastbuild'] + protos)
 
-  for rule in protos:
-    rule_dir  = rule.decode()[2:].rsplit(':')[0]
-    if language == "go":
-      input_dir = os.path.join(bazel_bin, rule_dir, 'pkg_go_proto_',
-                              'github.com/cncf/xds/go', rule_dir)
-      input_files = glob.glob(os.path.join(input_dir, '*.go'))
-    elif language == "python":
-      input_dir = os.path.join(bazel_bin, rule_dir)
-      input_files = glob.glob(os.path.join(input_dir, '*_pb2.py'))
-    
-    output_dir = os.path.join(output, rule_dir)
+def generate_lang_files_from_protos(language: str):
+    output = os.path.join(workspace, language)
+    bazel_bin = check_output(["bazel", "info", "bazel-bin"]).decode().strip()
 
-    # Ensure the output directory exists
-    os.makedirs(output_dir, 0o755, exist_ok=True)
-    for generated_file in input_files:
-      output_file = shutil.copy(generated_file, output_dir)
-      os.chmod(output_file, 0o644)
+    protos = (
+        check_output(
+            [
+                "bazel",
+                "query",
+                f'kind("{language}_proto_library", ...)',
+            ]
+        )
+        .decode()
+        .split()
+    )
+    output_dir = f"github.com/cncf/xds/{language}"
+    check_output(["bazel", "build", "-c", "fastbuild"] + protos)
+
+    for rule in protos:
+        rule_dir = rule.decode()[2:].rsplit(":")[0]
+        input_dir = {
+            "go": os.path.join(
+                bazel_bin, rule_dir, "pkg_go_proto_", "github.com/cncf/xds/go", rule_dir
+            ),
+            "python": os.path.join(bazel_bin, rule_dir),
+        }[language]
+        input_files = {
+            "go": glob.glob(os.path.join(input_dir, "*.go")),
+            "python": glob.glob(os.path.join(input_dir, "*_pb2.py")),
+        }[language]
+        if language == "go":
+            input_dir = os.path.join(
+                bazel_bin, rule_dir, "pkg_go_proto_", "github.com/cncf/xds/go", rule_dir
+            )
+            input_files = glob.glob(os.path.join(input_dir, "*.go"))
+        elif language == "python":
+            input_dir = os.path.join(bazel_bin, rule_dir)
+            input_files = glob.glob(os.path.join(input_dir, "*_pb2.py"))
+
+        output_dir = os.path.join(output, rule_dir)
+
+        # Ensure the output directory exists
+        os.makedirs(output_dir, 0o755, exist_ok=True)
+        for generated_file in input_files:
+            output_file = shutil.copy(generated_file, output_dir)
+            os.chmod(output_file, 0o644)
 
 
 if __name__ == "__main__":
-  workspace = check_output(['bazel', 'info', 'workspace']).decode().strip()
-  output = os.path.join(workspace, 'go')
-  generate_go_protobufs(output, "go")
-  output = os.path.join(workspace, 'python')
-  generate_go_protobufs(output, "python")
+    for language in SUPPORTED_LANGUAGES:
+        generate_lang_files_from_protos(language=language)
